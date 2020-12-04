3d7f637a0f21c17ad0eb15d189c1e4c7bd7f1e2c
Zdenko Klain <zdnkk@pm.me>
2020-12-04 01:36:38 +0100

    Removed boring pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import { Link } from "wouter";
 
 const Header = ({ style }) => (
   <Navbar style={style}>
-    <Link to="/">Box</Link>
+    <Link to="/">Waves</Link>
     {/* <Link to="/knot">Knot</Link> */}
-    <Link to="/gesture">Waves</Link>
+    <Link to="/box">Box</Link>
     <Link to="/text">text</Link>
-    <Link to="/cactus">cactus</Link>
+    {/* <Link to="/cactus">cactus</Link> */}
   </Navbar>
 );
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,42 +12,28 @@ const Pages = ({ portal = null, pageAnims, transition }) => {
   return transition(({ position, scale, opacity }, location) => (
     <a.group position={position} scale={scale}>
       <Switch location={location}>
-        <Route path="/gesture">
-          <Waves />
+        <Route path="/box">
+          {/* <Waves /> */}
+          <PageHeading animated={{ opacity }} portal={portal}>
+            Box
+          </PageHeading>
+          <BoxPage />
         </Route>
-        <Route path="/cactus">
+        {/* <Route path="/cactus">
           <PageHeading
             animated={{ opacity, color: pageAnims.color }}
             portal={portal}
           >
             Cactus
           </PageHeading>
-          <Cactus />
-        </Route>
+          <Cactus /> */}
+        {/* </Route> */}
         <Route path="/text">
           <Text>STRV</Text>
         </Route>
         <Route path="/">
-          <PageHeading animated={{ opacity }} portal={portal}>
-            Box
-          </PageHeading>
-
-          <BoxPage />
+          <Waves />
         </Route>
-        {/* <Route path="/knot">
-          <Juicy /> */}
-        {/* <PointsPage /> */}
-        {/* <PageHeading animated={{ opacity }} portal={portal}>
-            Knot
-          </PageHeading>
-          <TorusKnot scale={[1.5, 1.5, 1.5]}>
-            <meshStandardMaterial
-              roughness={0.2}
-              metalness={0.9}
-              color="yellow"
-            />
-          </TorusKnot> */}
-        {/* </Route> */}
       </Switch>
     </a.group>
   ));
