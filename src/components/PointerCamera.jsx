9c8f1d85ba661c9ec68045f104db0f696e19fb00
Zdenko Klain <zdnkk@pm.me>
2020-12-08 13:26:14 +0100

    Camera animated with spring

--- src/components/PointerCamera.jsx
+++ src/components/PointerCamera.jsx
@@ -1,21 +1,51 @@
-import { useEffect } from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import { useThree } from "react-three-fiber";
+import { Vector3 } from "three";
+import { useSpring } from "@react-spring/web";
+import { a, config } from "@react-spring/three";
 
 const PointerCamera = ({ disabled }) => {
-  const { camera, mouse, size } = useThree();
+  const ref = useRef();
 
-  // todo: smooth
+  const { camera, mouse, size, setDefaultCamera } = useThree();
+
+  const [animation, set] = useSpring(() => ({
+    rotation: [0, 0, 0],
+    config: config.molasses,
+  }));
+
+  useLayoutEffect(() => {
+    setDefaultCamera(ref.current);
+  }, [setDefaultCamera]);
+
   useEffect(() => {
     const handleMouseMove = (e) => {
       if (disabled) return;
 
-      camera.rotation.y = (-mouse.x / size.width) * 80;
-      camera.rotation.x = (mouse.y / size.height) * 80;
+      camera.rotation.setFromVector3(
+        new Vector3(-mouse.x / size.width, mouse.y / size.height, 0)
+      );
+
+      set({
+        rotation: [
+          (mouse.y / size.height) * 80,
+          (-mouse.x / size.width) * 80,
+          0,
+        ],
+      });
     };
     document.addEventListener("mousemove", handleMouseMove);
     return () => document.removeEventListener("mousemove", handleMouseMove);
-  }, [camera.rotation, size, mouse, disabled]);
-  return null;
+  }, [size, mouse, disabled]);
+
+  return (
+    <a.perspectiveCamera
+      ref={ref}
+      fov={75}
+      position={[0, 1, 10]}
+      {...animation}
+    />
+  );
 };
 
 export default PointerCamera;
